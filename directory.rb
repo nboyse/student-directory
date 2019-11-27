5d50ed3de481d25aaea66e18f4c0cea2101bb52e
nboyse <natasha.boyse@trade.gov.uk>
2019-11-27 15:25:10 +0000

    Updated with methods, local variables and more iteration

--- directory.rb
+++ directory.rb
@@ -9,13 +9,25 @@ students = ["Dr. Hannibal Lecter",
   "Freddy Krueger",
   "The Joker",
   "Joffrey Baratheon",
-  "Norman Bates"]
+  "Norman Bates"
+]
 
-#first we print the list of students
-puts "The students of Villains Academy"
-puts "-------------"
-students.each do |student|
-  puts student
+def print_header
+  puts "The students of Villains Academy"
+  puts "-------------"
 end
-# finally, we print the total number of students
-puts "Overall, we have #{students.count} great students"
+
+def print(names)
+  names.each do |name|
+    puts name
+  end
+end
+
+def print_footer(names)
+  puts "Overall, we have #{names.count} great students"
+end
+
+#nothing happens until the methods are called
+print_header
+print(students)
+print_footer(students)
